23a58ef616db90c134501a04b146bc3455f3e45c
markDeLaFleur <Mark.DeLaFleur@gmail.com>
2023-06-26 14:46:00 -0400

    working with images

--- app/src/main/java/com/markd/mxt_v2/MainActivity.kt
+++ app/src/main/java/com/markd/mxt_v2/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.markd.mxt_v2.ui.theme.MxT_v2Theme
 import org.opencv.android.OpenCVLoader
 
@@ -38,10 +46,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val imageModifier = Modifier
+        .size(10.dp)
+        .border(BorderStroke(1.dp, Color.White))
+        .background(Color.White)
+
+    Image(
+        painter = painterResource(id = R.drawable.ic_doublethree_wide),
+        contentDescription = "nookie",modifier =imageModifier
+
+    )
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+
 }
 
 @Preview(showBackground = true)
